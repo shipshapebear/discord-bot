a089c350184c569f3a25ce89504f10146be01535
Aaron Jay Malabanan <malabanan.aaronjay@gmail.com>
2023-07-26 13:12:16 +0800

    added file structure

--- src/index.js
+++ src/index.js
@@ -1,21 +1,74 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  IntentsBitField,
+} = require("discord.js");
 require("dotenv").config();
-const { Client, IntentsBitField } = require("discord.js");
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    GatewayIntentBits.Guilds,
   ],
 });
 
-client.on("ready", (e) => {
-  console.log(`${e.user.tag} is online.`);
-});
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.on("messageCreate", (message) => {
-  if (message.content === "murahin mo si jormiel") {
-    message.reply("Tang ina mo jormiel");
+  if (message.content.split(" ")[0] === "murahin") {
+    const name = message.content.split(" ")[3];
+    if (!name) {
+      message.reply('Your message should be "murahin mo si <name>"');
+    } else {
+      message.reply(`Tang ina mo ${name}`);
+    }
+  }
+
+  if (message.content === "cute ba si jormiel") {
+    message.reply("cute ampotangina sarap tirisin");
   }
 });
 
